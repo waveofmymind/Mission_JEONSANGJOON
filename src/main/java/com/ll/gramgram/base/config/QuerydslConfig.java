eb0ea1352b232d80f81b4ba8be19a4b1e1ce4e78
waveofmymind <sjun0913@gmail.com>
2023-04-04 16:38:27 +0900

    [FEAT] querydsl 설정 추가

---
+++ src/main/java/com/ll/gramgram/base/config/QuerydslConfig.java
@@ -0,0 +1,14 @@
+package com.ll.gramgram.base.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+}
