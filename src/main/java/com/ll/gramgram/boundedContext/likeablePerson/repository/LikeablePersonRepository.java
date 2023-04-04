b47db3914daea477d22f7b7fa9177d9248dbf279
waveofmymind <sjun0913@gmail.com>
2023-04-04 16:35:48 +0900

    [FIX] PK값 잘못 들어가있는 것 수정
    Integer -> Long

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -3,8 +3,6 @@ package com.ll.gramgram.boundedContext.likeablePerson.repository;
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Integer> {
+import java.util.List;public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Long> {
     List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
 }
